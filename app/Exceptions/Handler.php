85fe21d8f69f509e97cc1c1b4c723acdf26e03a0
ShoyimObloqulov <obloqulovshoyim1@gmail.com>
2024-03-30 00:13:12 -0700

    This folder contains all the methods needed to handle exceptions. It also contains the file handle.php that handles all the exceptions.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -24,7 +27,19 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if($e instanceof NotFoundHttpException) {
+                Log::info('Renderlash usulidan: '.$e->getMessage());
+
+                // you can return a view, json object, e.t.c
+                return response()->json([
+                    'message' => 'Renderlash usulidan: Resurs topilmadi'
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+            return response()->json([
+                'status' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
         });
     }
 }
